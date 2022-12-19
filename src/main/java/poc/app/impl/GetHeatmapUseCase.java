96f6ca34a37317a309d57a5d768123bb26b5c3d6
Ермаков Денис <DEErmakov@lanit.ru>
2022-12-19 12:59:41 +0300

    Координаты для heatmap берутся из bpmn-схемы

--- src/main/java/poc/app/impl/GetHeatmapUseCase.java
+++ src/main/java/poc/app/impl/GetHeatmapUseCase.java
@@ -1,14 +1,25 @@
 package poc.app.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import poc.app.api.ClasspathResource;
 import poc.app.api.GetHeatmapInbound;
-import poc.app.api.GetTaskListInbound;
-import poc.domain.bpmn.BpmnUserTask;
 import poc.domain.heatmap.DataPoint;
 import poc.domain.heatmap.Heatmap;
 
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+import java.io.InputStream;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -16,18 +27,91 @@ import java.util.List;
  */
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class GetHeatmapUseCase implements GetHeatmapInbound {
+
+    private final ClasspathResource classpathResource;
+
+    String ELEMENT_XPATH_EXPRESSION = "//bpmndi:BPMNShape[@bpmnElement='%s']/dc:Bounds";
 
     @Override
     public Heatmap execute() {
-        Heatmap heatmap = Heatmap.builder()
-            .min(0)
-            .max(100)
-            .data(List.of(
-                new DataPoint(330, 120, 30),
-                new DataPoint(750, 120, 70)
-            ))
-            .build();
-        return heatmap;
+
+        try {
+            Document xmlDocument = getXmlDocument();
+
+            XPathExpression inputDataNodeXPathExpression = getXPathExpression("input-data");
+            DataPoint inputDataElementDataPoint = getBpmnElementCenterCoords(xmlDocument, inputDataNodeXPathExpression);
+            XPathExpression processDataNodeXPathExpression = getXPathExpression("process-data-element");
+            DataPoint processDataElementDataPoint = getBpmnElementCenterCoords(xmlDocument, processDataNodeXPathExpression);
+
+            return Heatmap.builder()
+                .min(0)
+                .max(100)
+                .data(List.of(
+                    inputDataElementDataPoint,
+                    processDataElementDataPoint
+                ))
+                .build();
+
+        } catch (Exception e) {
+            log.error("", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Document getXmlDocument() throws Exception {
+        InputStream is = classpathResource.getBpmnFile().getInputStream();
+
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        builderFactory.setNamespaceAware(true);
+        DocumentBuilder builder = builderFactory.newDocumentBuilder();
+        return builder.parse(is);
+    }
+
+    private XPathExpression getXPathExpression(String elementId) throws Exception {
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        xPath.setNamespaceContext(new NamespaceContext() {
+            @Override
+            public Iterator getPrefixes(String arg0) {
+                return List.of("bpmndi").iterator();
+            }
+
+            @Override
+            public String getPrefix(String arg0) {
+                return null;
+            }
+
+            @Override
+            public String getNamespaceURI(String arg0) {
+                if ("bpmndi".equals(arg0)) {
+                    return "http://www.omg.org/spec/BPMN/20100524/DI";
+                } else if ("dc".equals(arg0)) {
+                    return "http://www.omg.org/spec/DD/20100524/DC";
+                }
+                return null;
+            }
+        });
+
+        String xPathExpression = String.format(ELEMENT_XPATH_EXPRESSION, elementId);
+        return xPath.compile(xPathExpression);
+    }
+
+    private DataPoint getBpmnElementCenterCoords(Document xmlDocument, XPathExpression xPathExpression) throws Exception {
+        Node elementNode = (Node) xPathExpression.evaluate(xmlDocument, XPathConstants.NODE);
+        NamedNodeMap attrs = elementNode.getAttributes();
+        String xStr = attrs.getNamedItem("x").getNodeValue();
+        String yStr = attrs.getNamedItem("y").getNodeValue();
+        String widthStr = attrs.getNamedItem("width").getNodeValue();
+        String heightStr = attrs.getNamedItem("height").getNodeValue();
+
+        int x = Integer.parseInt(xStr);
+        int y = Integer.parseInt(yStr);
+        int heigth = Integer.parseInt(heightStr);
+        int width = Integer.parseInt(widthStr);
+        int centerX = x + width / 2;
+        int centerY = y + heigth / 2;
+
+        return new DataPoint(centerX, centerY, 90);
     }
 }
